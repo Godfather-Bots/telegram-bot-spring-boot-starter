7d1e9370d1b272b824a5733132526d9510d49f52
Struchkov Mark <mark@struchkov.dev>
2023-02-18 11:52:42 +0300

    InitCommit

---
+++ src/main/java/dev/struchkov/godfather/telegram/starter/TelegramBotSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package dev.struchkov.godfather.telegram.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotSpringBootStarterApplication.class, args);
+	}
+
+}
